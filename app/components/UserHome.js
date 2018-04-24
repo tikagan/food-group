7dce42ad11658981be56ecdc0c1817811ef1a79d
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2018-04-24 16:58:01 -0400

    initial setup of UserHome

---
+++ app/components/UserHome.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import styles from './styles/UserHome.css';
+
+
+export default class UserHome extends Component {
+  render() {
+    return (
+      <div>
+        <div className={styles.container}>
+          <h2>UserHome</h2>
+          <Link to="/pantry">Pantry</Link>
+          <Link to="/grocerylists">GroceryLists</Link>
+        </div>
+      </div>
+    );
+  }
+}
