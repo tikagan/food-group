ba565fd1ac2dfdec22b0dbda97cb1cde2b2c500d
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2018-04-24 17:13:00 -0400

    routed new components

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
+import UserHomePage from './containers/UserHomePage';
 import CounterPage from './containers/CounterPage';
+import PantryPage from './containers/PantryPage';
+import GroceryListsPage from './containers/GroceryListsPage';
 
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={HomePage} />
+    <IndexRoute component={UserHomePage} />
     <Route path="/counter" component={CounterPage} />
+    <Route path="/pantry" component={PantryPage} />
+    <Route path="/grocerylists" component={GroceryListsPage} />
   </Route>
 );
