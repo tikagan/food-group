fa12d7ad5424b28446c6531cd054a21eaab0cc9a
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2018-04-24 16:58:43 -0400

    initial setup of Pantry

---
+++ app/components/Pantry.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+
+export default class Pantry extends Component {
+  render() {
+    return (
+      <div>
+        <div className="pantry">
+          <h2>Pantry</h2>
+        </div>
+      </div>
+    );
+  }
+}
